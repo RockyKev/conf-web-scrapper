f6955465cb56ec031d336adda7ac630de9b13edb
rockykev <kev.rocky@gmail.com>
2022-02-16 21:28:16 -0800

    more talks

--- node_scripts/get-youtube-playlist.js
+++ node_scripts/get-youtube-playlist.js
@@ -104,6 +104,21 @@ function getPresentationAuthor(theTitle, nameType) {
 
       break;
 
+      case "Presentation - Name, Venue":
+        if (theTitle.includes("-")) {
+          splitTitle = theTitle.split("-");
+          presentation = splitTitle[0].trim();
+
+          const splitName = splitTitle[1].split(",") // splitName[1] would be the venue
+          name = splitName[0].trim();
+
+        } else {
+          presentation = theTitle;
+          name = "NO AUTHOR NAME";
+        }
+  
+        break;
+  
     case "Presentation by Name":
       if (theTitle.includes("by")) {
         splitTitle = theTitle.split("by");
@@ -126,6 +141,8 @@ function getPresentationAuthor(theTitle, nameType) {
       }
 
       break;
+
+  
 
     case "Presentation":
     default:
@@ -180,16 +197,33 @@ function generateListObject(string) {
 
 
 
-const theOpenJSList = [
-  // generateListObject("OpenJS~~~OpenJS Foundation Collaborator Summit, Berlin~~~2019~~~Presentation~~~PLyspMSh4XhLMAIqlh3Z5R6frHMDc7t3eG"),
-  generateListObject("OpenJS~~~Node + JS Interactive~~~2019~~~Presentation - Name~~~PLyspMSh4XhLPKZxHu3ZzbUXO4WW-40g17"),
-  // generateListObject("OpenJS~~~OpenJS World~~~2020~~~Presentation - Name~~~PLyspMSh4XhLP-mqulUMcaqTbLo-ZJxSX5"),
-  // generateListObject("OpenJS~~~OpenJS World~~~2021~~~Presentation - Name~~~PLyspMSh4XhLNU9RWjXqdNOp3NXM3NWdF_"),
-];
+// const theOpenJSList = [
+//   generateListObject("OpenJS~~~OpenJS Foundation Collaborator Summit, Berlin~~~2019~~~Presentation~~~PLyspMSh4XhLMAIqlh3Z5R6frHMDc7t3eG"),
+//   generateListObject("OpenJS~~~Node + JS Interactive~~~2019~~~Presentation - Name~~~PLyspMSh4XhLPKZxHu3ZzbUXO4WW-40g17"),
+//   generateListObject("OpenJS~~~OpenJS World~~~2020~~~Presentation - Name~~~PLyspMSh4XhLP-mqulUMcaqTbLo-ZJxSX5"),
+//   generateListObject("OpenJS~~~OpenJS World~~~2021~~~Presentation - Name~~~PLyspMSh4XhLNU9RWjXqdNOp3NXM3NWdF_"),
+// ];
 
+const theGitNationList = [
+  generateListObject("JavaScript Conferences by GitNation~~~AmsterdamJS Conference~~~2017~~~Presentation - Name~~~PLfIM4SvaiIyzaLhvwGEa4QzPb9oTD0Ioc"),
+  generateListObject("JavaScript Conferences by GitNation~~~JSNation Conference~~~2018~~~Presentation - Name~~~PLfIM4SvaiIywIr04DZs6NHNq_G4S_2hro"),
+  generateListObject("JavaScript Conferences by GitNation~~~JSNation Conference~~~2019~~~Presentation - Name~~~PLfIM4SvaiIyygQEe2WPpENwxIf-0agBr9"),
+  generateListObject("JavaScript Conferences by GitNation~~~JavaScript Frontend Talks~~~2020~~~Presentation - Name~~~PLfIM4SvaiIyxT5-wqb5o9lucx84mXy0qU"),
+  generateListObject("JavaScript Conferences by GitNation~~~TyepScript Talks~~~2020~~~Presentation - Name~~~PLfIM4SvaiIyxn9WDU5v15KKsJbRqluoH_"),
+  generateListObject("JavaScript Conferences by GitNation~~~JavaScript Testing Talks~~~2021~~~Presentation - Name~~~PLfIM4SvaiIyxZWVuZZr_lRvLa9E2mRBgq"),
+  generateListObject("JavaScript Conferences by GitNation~~~Node.js Talks~~~2021~~~Presentation - Name~~~PLfIM4SvaiIyzgM4KAW2cejoq2-nzEaSfQ"),
+  generateListObject("JavaScript Conferences by GitNation~~~JavaScript API Talks~~~2020~~~Presentation - Name~~~PLfIM4SvaiIyyYPn-in97UR_sHtQt_eNmG"),
+  generateListObject("JavaScript Conferences by GitNation~~~JSNation Live Conference~~~2020~~~Presentation - Name~~~PLfIM4SvaiIyySUvx7L8PXNl5nqM9x_dXC"),
+  generateListObject("JavaScript Conferences by GitNation~~~TestJS Summit~~~2021~~~Presentation - Name~~~PLfIM4SvaiIyyIfaGCY6c5bt0EBVV_hXGy"),
+  generateListObject("JavaScript Conferences by GitNation~~~Node Congress~~~2021~~~Presentation - Name~~~PLfIM4SvaiIyxqwCkTyBfo2NjBbacK-eJS"),
+  generateListObject("JavaScript Conferences by GitNation~~~DevOps.js~~~2021~~~Presentation - Name~~~PLfIM4SvaiIyxBv7CF4VZMlE3F7GTz6ssI"),
+  generateListObject("JavaScript Conferences by GitNation~~~JSNation Live Conference~~~2021~~~Presentation - Name~~~PLfIM4SvaiIyyQUbLvImHltKD1dCp6YH9u"),
+  generateListObject("JavaScript Conferences by GitNation~~~VUe.js Live~~~2021~~~Presentation - Name, Venue~~~PLfIM4SvaiIyyRQB2Ga9YxNxGFRSmtSeZ9"),
+  generateListObject("JavaScript Conferences by GitNation~~~TestJS Summit~~~2021~~~Presentation - Name, Venue~~~PLfIM4SvaiIywKQCWi9Pd6WtGd8Q8C5rOe"),
+]
 
 // This is the code that always fire
-const makeExecute = theOpenJSList;
+const makeExecute = theGitNationList;
 
 
 async function sleep(millis) {
