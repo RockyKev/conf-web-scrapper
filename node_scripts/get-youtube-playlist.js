d71d61b9172c5a3fad31524c6ceaca9a587d04fa
rockykev <kev.rocky@gmail.com>
2022-02-23 23:12:02 -0800

    moved node code

--- node_scripts/get-youtube-playlist.js
+++ node_scripts/get-youtube-playlist.js
@@ -1,10 +1,21 @@
 const ytpl = require("ytpl"); // https://www.npmjs.com/package/ytpl
 const { generateFile } = require("./utilities");
-const { toTitleCase, destructureIntoPlaylistObject } = require("./helpers");
+const { toTitleCase, destructureIntoPlaylistObject, getPresentationAuthor } = require("./helpers");
 
 
 // import files
-const { jsCongressList } = require("../video_data/data_objects/jsKongressList")
+// const { jsCongressList } = require("../video_data/data_objects/jsKongressList")
+const { jamStackList } = require("../video_data/data_objects/jamStackList") // not yet imported
+const { internationalJSList } = require("../video_data/data_objects/internationalJSList") // not yet imported
+const { gitNationList2022_02_23 } = require("../video_data/data_objects/gitNationList") // not yet imported
+const { jsFestList } = require("../video_data/data_objects/jsFestList") // not yet imported
+const { jsHeroesList } = require("../video_data/data_objects/jsHeroesList") // not yet imported
+const { grUSPjsdayList } = require("../video_data/data_objects/grUSPjsdayList") // not yet imported
+const { jsCampList } = require("../video_data/data_objects/jsCampList") // not yet imported
+
+const CURRENT_PLAYLIST = '';
+const DEBUG_RUN_ONE = false;
+
 
 // TODO:
 // 1 - migrate the async and clean data call out
@@ -80,138 +91,166 @@ function cleanTheData(data, titleOwner, title, year, namingType) {
   };
 }
 
-function getPresentationAuthor(nameTitle, nameType) {
-  let name, presentation, splitTitle;
-  let theTitle = nameTitle;
+// TODO: DElete this
+// function getPresentationAuthor(nameTitle, nameType) {
+//   let name, presentation, splitTitle;
+//   let theTitle = nameTitle;
 
-  // use case with this dash "–"
-  if (theTitle.includes("–")) {
-    theTitle = theTitle.replaceAll("–", "-");
-  }
+//   // use case with this dash "–"
+//   if (theTitle.includes("–")) {
+//     theTitle = theTitle.replaceAll("–", "-");
+//   }
 
-  // TODO: figure out how to get the last element in this pattern
-  // break-dancing - john smith
+//   // TODO: figure out how to get the last element in this pattern
+//   // break-dancing - john smith
 
-  switch (nameType) {
-    case "Name - Presentation":
-      if (theTitle.includes("-")) {
-        splitTitle = theTitle.split("-");
-        name = splitTitle[0].trim();
-        presentation = splitTitle[1].trim();
-      } else {
-        presentation = theTitle;
-        name = "NO_AUTHOR_NAME";
-      }
-      break;
-    case "Presentation - Name":
-      if (theTitle.includes("-")) {
-        // if the array is more than 2
-        splitTitle = theTitle.split("-");
+//   switch (nameType) {
+//     case "Name - Presentation":
+//     case "Name - Presentation - Venue":
+//     case "Venue | Name - Presentation":
+//       if (theTitle.includes("-")) {
+//         splitTitle = theTitle.split("-");
+//         name = splitTitle[0].trim();
+//         presentation = splitTitle[1].trim();
+//       } else {
+//         presentation = theTitle;
+//         name = "NO_AUTHOR_NAME";
+//       }
+//       break;
 
-        if (splitTitle.length > 2) {
-          name = splitTitle.pop();
-          presentation = splitTitle.join(" ");
-        } else {
-          presentation = splitTitle[0].trim();
-          name = splitTitle[1].trim();
-        }
-      } else {
-        presentation = theTitle;
-        name = "NO_AUTHOR_NAME";
-      }
+//     case "Presentation - Name":
+//     case "Presentation - Name | Venue":
+//       if (theTitle.includes("-")) {
+//         // if the array is more than 2
+//         splitTitle = theTitle.split("-");
 
-      break;
+//         if (splitTitle.length > 2) {
+//           name = splitTitle.pop();
+//           presentation = splitTitle.join(" ");
+//         } else {
+//           presentation = splitTitle[0].trim();
+//           name = splitTitle[1].trim();
+//         }
+//       } else {
+//         presentation = theTitle;
+//         name = "NO_AUTHOR_NAME";
+//       }
 
-    case "Presentation - Name, Venue":
-      if (theTitle.includes("-")) {
-        splitTitle = theTitle.split("-");
-        presentation = splitTitle[0].trim();
+//       break;
 
-        const splitName = splitTitle[1].split(","); // splitName[1] would be the venue
-        name = splitName[0].trim();
-      } else {
-        presentation = theTitle;
-        name = "NO_AUTHOR_NAME";
-      }
+//     case "Presentation - Name, Venue":
+//       if (theTitle.includes("-") && theTitle.includes(",")) {
+//         splitTitle = theTitle.split("-");
+//         presentation = splitTitle[0].trim();
 
-      break;
+//         const splitName = splitTitle[1].split(","); // splitName[1] would be the venue
+//         name = splitName[0].trim();
+//       } else {
+//         presentation = theTitle;
+//         name = "NO_AUTHOR_NAME";
+//       }
 
-    case "Presentation by Name":
-      if (theTitle.includes("by")) {
-        splitTitle = theTitle.split("by");
-        presentation = splitTitle[0].trim();
-        name = splitTitle[1].trim();
-      } else {
-        presentation = theTitle;
-        name = "NO_AUTHOR_NAME";
-      }
+//       break;
 
-      break;
+//     case "Presentation by Name":
+//       if (theTitle.includes("by")) {
+//         splitTitle = theTitle.split("by");
+//         presentation = splitTitle[0].trim();
+//         name = splitTitle[1].trim();
+//       } else {
+//         presentation = theTitle;
+//         name = "NO_AUTHOR_NAME";
+//       }
 
-    case "Presentation | Name":
-      if (theTitle.includes("|")) {
-        splitTitle = theTitle.split("|");
-        presentation = splitTitle[0].trim();
-        name = splitTitle[1].trim();
-      } else {
-        presentation = theTitle;
-        name = "NO_AUTHOR_NAME";
-      }
+//       break;
 
-      break;
+//     case "Presentation | Name":
+//     case "Presentation | Name | Venue":
+//     if (theTitle.includes("|")) {
+//         splitTitle = theTitle.split("|");
+//         presentation = splitTitle[0].trim();
+//         name = splitTitle[1].trim();
+//       } else {
+//         presentation = theTitle;
+//         name = "NO_AUTHOR_NAME";
+//       }
 
-    case "Name: Presentation - Title":
-      if (theTitle.includes(":") && theTitle.includes("-")) {
-
-        splitTitle = theTitle.split(":");
-        let presentationTemp = splitTitle[1].split('-'); 
-
-        presentation = presentationTemp[0].trim();
-        name = splitTitle[0].trim();
-
-      } else {
-        presentation = theTitle;
-        name = "NO_AUTHOR_NAME";
-      }
-
-      break;
-
-
-      case "Name: Presentation":
-        if (theTitle.includes(":")) {
+//       break;
   
-          splitTitle = theTitle.split(":");
-  
-          presentation = splitTitle[1].trim();
-          name = splitTitle[0].trim();
-  
-        } else {
-          presentation = theTitle;
-          name = "NO_AUTHOR_NAME";
-        }
-  
-        break;
 
-    case "Presentation":
-    default:
-      presentation = theTitle;
-      name = "NO_AUTHOR_NAME";
-  }
+//     case "Name: Presentation - Venue":
+//       if (theTitle.includes(":") && theTitle.includes("-")) {
 
-  return {
-    author: toTitleCase(name),
-    title: presentation,
-  };
-}
+//         splitTitle = theTitle.split(":");
+//         let presentationTemp = splitTitle[1].split('-'); 
 
-// TODO: turn this into export files that you import in
+//         presentation = presentationTemp[0].trim();
+//         name = splitTitle[0].trim();
+
+//       } else {
+//         presentation = theTitle;
+//         name = "NO_AUTHOR_NAME";
+//       }
+
+//       break;
+
+
+//     case "Name: Presentation":
+//       if (theTitle.includes(":")) {
+
+//         splitTitle = theTitle.split(":");
+
+//         presentation = splitTitle[1].trim();
+//         name = splitTitle[0].trim();
+
+//       } else {
+//         presentation = theTitle;
+//         name = "NO_AUTHOR_NAME";
+//       }
+
+//       break;
+
+//     case "Presentation. Name. Venue":
+//       if (theTitle.includes(".")) {
+//         splitTitle = theTitle.split(".");
+//         presentation = splitTitle[0].trim();
+//         name = splitTitle[1].trim();
+//       } else {
+//         presentation = theTitle;
+//         name = "NO_AUTHOR_NAME";
+//       }
+      
+
+//       break;
+
+//   case "Presentation? Name":
+//     if (theTitle.includes("?")) {
+//       splitTitle = theTitle.split("?");
+//       presentation = splitTitle[0].trim();
+//       name = splitTitle[1].trim();
+//     } else {
+//       presentation = theTitle;
+//       name = "NO_AUTHOR_NAME";
+//     }
+    
+
+//     break;
+        
+//     case "Presentation":
+//     default:
+//       presentation = theTitle;
+//       name = "NO_AUTHOR_NAME";
+//   }
+
+//   return {
+//     author: toTitleCase(name),
+//     title: presentation,
+//   };
+// }
 
 // This is the code that always fire
 
 // THE ACTUAL CODE
-
-const CURRENT_PLAYLIST = jsCongressList;
-const DEBUG_RUN_ONE = false;
 
 async function sleep(millis) {
   return new Promise((resolve) => setTimeout(resolve, millis));
--- node_scripts/helpers.js
+++ node_scripts/helpers.js
@@ -1,41 +1,190 @@
 // This file is more for string things and object generation
 
 function destructureIntoPlaylistObject(string) {
-    const splitString = string.split("~~~");
-    const conferenceOwner = splitString[0].trim();
-    const conference = splitString[1].trim();
-    const year = splitString[2].trim();
-    const namePattern = splitString[3].trim();
-    const playListId = splitString[4].trim();
-  
-    return {
-      playlistId: playListId,
-      conferenceOwner: conferenceOwner,
-      conference: conference,
-      year: year,
-      namingType: namePattern,
-    };
-  
-    // this isn't used yet
-    // const masterList = [{
-    //   playlistId: "PL0TQYXcAtbwSh9ZbY-34J9XK2fVR23aCl",
-    //   conferenceOwner: "International JavaScript Conference",
-    //   conference: "International JavaScript Conference",
-    //   year: 2019,
-    //   namingType: 'Presentation | Name',
-    // }
-    // ]
-  }
+  const splitString = string.split("~~~");
+  const conferenceOwner = splitString[0].trim();
+  const conference = splitString[1].trim();
+  const year = splitString[2].trim();
+  const namePattern = splitString[3].trim();
+  const playListId = splitString[4].trim();
+
+  return {
+    playlistId: playListId,
+    conferenceOwner: conferenceOwner,
+    conference: conference,
+    year: year,
+    namingType: namePattern,
+  };
+
+  // this isn't used yet
+  // const masterList = [{
+  //   playlistId: "PL0TQYXcAtbwSh9ZbY-34J9XK2fVR23aCl",
+  //   conferenceOwner: "International JavaScript Conference",
+  //   conference: "International JavaScript Conference",
+  //   year: 2019,
+  //   namingType: 'Presentation | Name',
+  // }
+  // ]
+}
 
 const toTitleCase = (phrase) => {
-    return phrase
-      .toLowerCase()
-      .split(' ')
-      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
-      .join(' ');
-  };
-  
-  module.exports = {
-      toTitleCase, 
-      destructureIntoPlaylistObject
+  return phrase
+    .toLowerCase()
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
+};
+
+function getPresentationAuthor(nameTitle, nameType) {
+  let name, presentation, splitTitle;
+  let theTitle = nameTitle;
+
+  // use case with this dash "–"
+  if (theTitle.includes("–")) {
+    theTitle = theTitle.replaceAll("–", "-");
   }
+
+  // TODO: figure out how to get the last element in this pattern
+  // break-dancing - john smith
+
+  switch (nameType) {
+    case "Name - Presentation":
+    case "Name - Presentation - Venue":
+    case "Venue | Name - Presentation":
+      if (theTitle.includes("-")) {
+        splitTitle = theTitle.split("-");
+        name = splitTitle[0].trim();
+        presentation = splitTitle[1].trim();
+      } else {
+        presentation = theTitle;
+        name = "NO_AUTHOR_NAME";
+      }
+      break;
+
+    case "Presentation - Name":
+    case "Presentation - Name | Venue":
+      if (theTitle.includes("-")) {
+        // if the array is more than 2
+        splitTitle = theTitle.split("-");
+
+        if (splitTitle.length > 2) {
+          name = splitTitle.pop();
+          presentation = splitTitle.join(" ");
+        } else {
+          presentation = splitTitle[0].trim();
+          name = splitTitle[1].trim();
+        }
+      } else {
+        presentation = theTitle;
+        name = "NO_AUTHOR_NAME";
+      }
+
+      break;
+
+    case "Presentation - Name, Venue":
+      if (theTitle.includes("-") && theTitle.includes(",")) {
+        splitTitle = theTitle.split("-");
+        presentation = splitTitle[0].trim();
+
+        const splitName = splitTitle[1].split(","); // splitName[1] would be the venue
+        name = splitName[0].trim();
+      } else {
+        presentation = theTitle;
+        name = "NO_AUTHOR_NAME";
+      }
+
+      break;
+
+    case "Presentation by Name":
+      if (theTitle.includes("by")) {
+        splitTitle = theTitle.split("by");
+        presentation = splitTitle[0].trim();
+        name = splitTitle[1].trim();
+      } else {
+        presentation = theTitle;
+        name = "NO_AUTHOR_NAME";
+      }
+
+      break;
+
+    case "Presentation | Name":
+    case "Presentation | Name | Venue":
+      if (theTitle.includes("|")) {
+        splitTitle = theTitle.split("|");
+        presentation = splitTitle[0].trim();
+        name = splitTitle[1].trim();
+      } else {
+        presentation = theTitle;
+        name = "NO_AUTHOR_NAME";
+      }
+
+      break;
+
+    case "Name: Presentation - Venue":
+      if (theTitle.includes(":") && theTitle.includes("-")) {
+        splitTitle = theTitle.split(":");
+        let presentationTemp = splitTitle[1].split("-");
+
+        presentation = presentationTemp[0].trim();
+        name = splitTitle[0].trim();
+      } else {
+        presentation = theTitle;
+        name = "NO_AUTHOR_NAME";
+      }
+
+      break;
+
+    case "Name: Presentation":
+      if (theTitle.includes(":")) {
+        splitTitle = theTitle.split(":");
+
+        presentation = splitTitle[1].trim();
+        name = splitTitle[0].trim();
+      } else {
+        presentation = theTitle;
+        name = "NO_AUTHOR_NAME";
+      }
+
+      break;
+
+    case "Presentation. Name. Venue":
+      if (theTitle.includes(".")) {
+        splitTitle = theTitle.split(".");
+        presentation = splitTitle[0].trim();
+        name = splitTitle[1].trim();
+      } else {
+        presentation = theTitle;
+        name = "NO_AUTHOR_NAME";
+      }
+
+      break;
+
+    case "Presentation? Name":
+      if (theTitle.includes("?")) {
+        splitTitle = theTitle.split("?");
+        presentation = splitTitle[0].trim();
+        name = splitTitle[1].trim();
+      } else {
+        presentation = theTitle;
+        name = "NO_AUTHOR_NAME";
+      }
+
+      break;
+
+    case "Presentation":
+    default:
+      presentation = theTitle;
+      name = "NO_AUTHOR_NAME";
+  }
+
+  return {
+    author: toTitleCase(name),
+    title: presentation,
+  };
+}
+
+module.exports = {
+  toTitleCase,
+  destructureIntoPlaylistObject,
+  getPresentationAuthor
+};
