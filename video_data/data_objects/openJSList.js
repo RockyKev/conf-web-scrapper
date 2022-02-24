b3233a3e6c65fd75ecc1bb2f6f4f31e7e42a414c
rockykev <kev.rocky@gmail.com>
2022-02-23 22:20:46 -0800

    fix data

--- video_data/data_objects/openJSList.js
+++ video_data/data_objects/openJSList.js
@@ -1,7 +1,13 @@
+const { destructureIntoPlaylistObject } = require("../../node_scripts/helpers");
+
 const theOpenJSList = [
-    generateListObject("OpenJS~~~OpenJS Foundation Collaborator Summit, Berlin~~~2019~~~Presentation~~~PLyspMSh4XhLMAIqlh3Z5R6frHMDc7t3eG"),
-    generateListObject("OpenJS~~~Node + JS Interactive~~~2019~~~Presentation - Name~~~PLyspMSh4XhLPKZxHu3ZzbUXO4WW-40g17"),
-    generateListObject("OpenJS~~~OpenJS World~~~2020~~~Presentation - Name~~~PLyspMSh4XhLP-mqulUMcaqTbLo-ZJxSX5"),
-    generateListObject("OpenJS~~~OpenJS World~~~2021~~~Presentation - Name~~~PLyspMSh4XhLNU9RWjXqdNOp3NXM3NWdF_"),
+  destructureIntoPlaylistObject("OpenJS~~~OpenJS Foundation Collaborator Summit, Berlin~~~2019~~~Presentation~~~PLyspMSh4XhLMAIqlh3Z5R6frHMDc7t3eG"),
+  destructureIntoPlaylistObject("OpenJS~~~Node + JS Interactive~~~2019~~~Presentation - Name~~~PLyspMSh4XhLPKZxHu3ZzbUXO4WW-40g17"),
+  destructureIntoPlaylistObject("OpenJS~~~OpenJS World~~~2020~~~Presentation - Name~~~PLyspMSh4XhLP-mqulUMcaqTbLo-ZJxSX5"),
+  destructureIntoPlaylistObject("OpenJS~~~OpenJS World~~~2021~~~Presentation - Name~~~PLyspMSh4XhLNU9RWjXqdNOp3NXM3NWdF_"),
   ];
   
+  
+module.exports = {
+  theOpenJSList
+}
